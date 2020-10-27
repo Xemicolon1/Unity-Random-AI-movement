e655acbe75acf127f1866ab84fcb22f3ad4f2637
Xemicolon1 <73455079+Xemicolon1@users.noreply.github.com>
2020-10-27 10:02:00 +0530

    Scripts

---
+++ Scripts/Ai.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public float radius;
+
+    private void Start ()
+    {
+        agent = GetComponent<NavMeshAgent> ();
+    }
+
+    private void Update ()
+    {
+        if (!agent.hasPath)
+        {
+            agent.SetDestination (GetPoint.Instance.GetRandomPoint (transform, radius));
+        }
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos ()
+    {
+        Gizmos.DrawWireSphere (transform.position, radius);
+    }
+
+#endif
+}
